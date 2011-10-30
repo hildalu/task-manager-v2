cf466853a54baa347c41e8ba3ca344efd4cd0f83
Innovation <innovation@ubuntu.(none)>
2011-10-30 11:59:04 -0700

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskManagerV2::Application.config.secret_token = 'f1f137529af9008b978877fb6d51c6120b6e1da82d5be1a5ac6ce7d3e9721dde1de3c63a9832cd7a7acd4d7cc04bd03f70ef0f68a29eb4ec14526cc00202fc10'
